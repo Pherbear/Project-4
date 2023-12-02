bb06a3b7aa104ebeca0792eb3231c94f1575ffa8
Bryan <bryan.garcia.cuevas@gmail.com>
2023-12-02 14:08:54 -0800

    updated init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import PocketBase from 'pocketbase'
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
