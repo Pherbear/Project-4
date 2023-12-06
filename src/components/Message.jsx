ec936cdba0f4625a751f48e356b0860d61b23a04
Bryan1000 <brivas100@hotmail.com>
2023-12-05 18:38:33 -0800

    Time Stamp Update

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,13 +4,29 @@ const Message = ({ message_data }) => {
   const { message, created, user } = message_data;
 
   return (
-    <div className="message">
-      <p>
-        <strong>{user}</strong>: {message}
-      </p>
-      <p className="timestamp">{created}</p>
-    </div>
+    <div style={styles.messageContainer}>
+      <div style={styles.message}>
+        <p>
+          <strong>{user}</strong>: {message}   
+        </p>
+      </div>
+        {<p style={styles.timestamp}>{created}</p>}
+      </div>
   );
+  }
+  const styles = {
+    messageContainer: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      marginBottom: '10px'
+    },
+    
+    timestamp: {
+      textAlign: 'right',
+      color: '#888', 
+      fontSize: '12px' 
+    },
 };
 
 export default Message;
+
