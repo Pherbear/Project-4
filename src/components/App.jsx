17e88c8b9a6c7cbd901ae4559a70bcdb864238fc
Bryan <bryan.garcia.cuevas@gmail.com>
2023-12-05 18:05:06 -0800

    did some more user management stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom/client';
 import MessageList from './MessageList';
 import MessageInput from './MessageInput';
 import Chat from "./Chat";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 const USERNAME = process.env.REACT_APP_POCKETBASE_USERNAME
 const PASSWORD = process.env.REACT_APP_POCKETBASE_PASSWORD
 
 const pb = new PocketBase(`http://127.0.0.1:8090`)
 const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD)
-
-const currentUser_ID = 'aywlc8y4yt5naj9'
 
 async function getAllMessages(){
   const records = await pb.collection('messages').getFullList({
@@ -38,7 +38,7 @@ async function getUserName(userID){
     expand: 'relField1,relField2.subRelField',
   });
 
-  return record.name
+  return record.username
 }
 
 async function getUsers(){
@@ -49,27 +49,68 @@ async function getUsers(){
   return records
 }
 
-
 const allUsers = await getUsers()
 
 function App() {
-  
+  const [currentUser, setCurrentUser] = useState(
+    pb.authStore.isValid? pb.authStore.baseModel : ''
+  )
+
+  console.log(currentUser)
+
   const [messages, setMessages] = useState(load_data);
   const [users, setUsers] = useState(allUsers)
   
+  async function onSignUp(login_info){
+    console.log(login_info)
+
+    const {name, username, password, email} = login_info
+
+    const data = {
+      "username": username,
+      "email": email,
+      "emailVisibility": true,
+      "password": password,
+      "passwordConfirm": password,
+      "name": name
+    };
+
+    const record = await pb.collection('users').create(data);
+    login(email, password)
+  }
+
+  function logout(){
+    pb.authStore.clear()
+    setCurrentUser('')
+  }
+
+  async function login(email, password){
+    const authData = await pb.collection('users').authWithPassword(
+      email,
+      password,
+    );
+
+    if (pb.authStore.isValid) {
+      setCurrentUser(pb.authStore.baseModel)
+    } else {
+      console.log("Login Failed!")
+    }
+
+    setUsers([...users, currentUser])
+  }
+
   const addMessage = async (message_data) => {
     console.log(message_data)
 
     const data = {
-      "relation": currentUser_ID,
+      "relation": currentUser.id,
       "message": message_data.message,
-      "user": await getUserName(currentUser_ID)
+      "user": await getUserName(currentUser.id)
     };
     
     const record = await pb.collection('messages').create(data);
     setMessages([...messages, record])
   };
-
 
   function clearLog() {
     clearMessages()
@@ -78,8 +119,18 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello, {authData.admin.email}</h1>
-      <h1>Messages: </h1>
+      {
+        currentUser? 
+          <div> 
+            <a>logged in</a>
+            <button onClick={logout}>Logout</button> 
+          </div> 
+        : <div>
+            <a>Not Logged In</a>
+            <button onClick={login}>Login/Signup</button>
+            <SignUp onSignUp={onSignUp}/>
+          </div>
+      }
 
       <Chat 
         messages={messages} 
--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -2,15 +2,26 @@ import React, { useState } from 'react';
 
 const SignUp = ({ onSignUp }) => {
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('')
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   };
 
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,11 +29,13 @@ const SignUp = ({ onSignUp }) => {
     // Perform validation (you can add more validation logic here)
 
     // Call the onSignUp function with the user information
-    onSignUp({ username, password });
+    onSignUp({ username, email, password, name });
 
     // Clear the form fields after submission
     setUsername('');
     setPassword('');
+    setEmail('')
+    setName('')
   };
 
   return (
@@ -30,12 +43,32 @@ const SignUp = ({ onSignUp }) => {
       <h2>Sign Up</h2>
       <form onSubmit={handleSubmit}>
         <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+        </div>
+        <div>
           <label htmlFor="username">Username:</label>
           <input
             type="text"
             id="username"
             value={username}
             onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
             required
           />
         </div>
