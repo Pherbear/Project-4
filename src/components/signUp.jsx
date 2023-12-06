c140b2038c22827ef3b273967ffd02e2eb3e643c
Bryan1000 <brivas100@hotmail.com>
2023-12-05 16:56:03 -0800

    signUp component

---
+++ src/components/signUp.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const SignUp = ({ onSignUp }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // Perform validation (you can add more validation logic here)
+
+    // Call the onSignUp function with the user information
+    onSignUp({ username, password });
+
+    // Clear the form fields after submission
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
